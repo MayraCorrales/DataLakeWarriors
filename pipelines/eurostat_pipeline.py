23ca96b5d87762775632b023e51ab7d2b51d31d4
MayraCorrales <mayracorrales@icloud.com>
2026-03-16 15:36:46 +0100

    Update eurostat_pipeline.py

--- pipelines/eurostat_pipeline.py
+++ pipelines/eurostat_pipeline.py
@@ -1,1 +1,18 @@
+import requests
+import json
 
+# Example Eurostat dataset
+url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/une_rt_m"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    with open("eurostat_data.json", "w") as f:
+        json.dump(data, f)
+
+    print("Data downloaded successfully")
+
+else:
+    print("Error downloading data")
